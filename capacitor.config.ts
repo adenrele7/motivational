de24d7b5a57aa9931c121087bfbae32082b0e54b
adenrele7 <adenreleadepoju36@gmail.com>
2024-08-19 05:16:55 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Motivational',
+  webDir: 'dist'
+};
+
+export default config;
